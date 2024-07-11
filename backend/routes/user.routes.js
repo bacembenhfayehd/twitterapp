574503daa5caf077d042cb4db927d3bfa60e30e1
Bacem <Bacem@gmail.com>
2024-07-11 18:26:04 +0100

    usercontrollers2

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import { protectRoute } from '../middlewares/protectRoute.js';
-import { getUserProfile , followUnfollowUser } from '../controllers/user.controller.js';
+import { getUserProfile , followUnfollowUser , getSuggestedUsers, updateUser } from '../controllers/user.controller.js';
 
 
 const router = express.Router();
 
 router.get('/profile/:username',protectRoute,getUserProfile)
 router.post('/follow/:id',protectRoute,followUnfollowUser)
+router.get('/suggested',protectRoute,getSuggestedUsers)
+router.post('/update' , protectRoute,updateUser);
 
 export default router ;
 
