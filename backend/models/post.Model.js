9702c76338e26769ac401cce2b497834c1abc4b2
Bacem <Bacem@gmail.com>
2024-07-17 10:27:01 +0100

    add postModel

---
+++ backend/models/post.Model.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema (
+
+    {
+        User : {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : 'User',
+            required : true
+        },
+
+        text : {
+            type : String,
+        },
+
+        img : {
+            type : String,
+        },
+
+        likes : [
+            {
+                type : mongoose.Schema.Types.ObjectId,
+            ref : 'User',
+            }
+        ],
+
+        comments : [
+            {
+                text : {
+                    type : String,
+                },
+
+                user:{
+                    type : mongoose.Schema.Types.ObjectId,
+             ref : 'User',
+            required : true,
+
+                },
+            },
+        ],
+
+
+},
+    
+ {timestamps:true});
+
+
+ const Post = mongoose.model('Post' , postSchema);
+ export default Post;
