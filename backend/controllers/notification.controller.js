90934a3aad17974d14549c61ee252b8e4953a857
Bacem <Bacem@gmail.com>
2024-08-11 14:02:58 +0100

    finish backend logic

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -29,3 +29,32 @@ export const deleteNotifications = async (req, res) => {
     console.log("error deleting notifications", error);
   }
 };
+
+
+export const deleteNotification = async (req,res) => {
+
+try {
+
+  const notificationId = req.params.id;
+  const userId = req.user._id;
+  const notification = await Notifcation.findById(notificationId);
+if (!notification){
+  return res.status(404).json({error:'notification not found'})
+}
+
+if(notification.to.toString() !== userId.toString()){
+  return res.status(403).json({error:'you are not allowed to delete this notification'})
+}
+
+await Notifcation.findByIdAndDelete(notificationId);
+res.status(200).json({message:'notification deleted successfully'})
+
+} catch (error) {
+
+  res.status(500).json({ error: "Server error" });
+    console.log("error deleting this notification", error);
+  
+}
+
+
+}
--- backend/routes/notification.routes.js
+++ backend/routes/notification.routes.js
@@ -4,11 +4,13 @@ import { protectRoute } from "../middlewares/protectRoute.js";
 import {
   deleteNotifications,
   getNotifications,
+  deleteNotification
 } from "../controllers/notification.controller.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, getNotifications);
 router.delete("/", protectRoute, deleteNotifications);
+router.delete("/:id", protectRoute,deleteNotification);
 
 export default router;
