bb7041a2d37bde416b8e1292b418b7d147695a2a
Bacem <Bacem@gmail.com>
2024-08-14 17:02:30 +0100

    payload limit

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 console.log(process.env.MONGO_URI);
 
-app.use(express.json());
+app.use(express.json({limit:'5mb'}));
 app.use(express.urlencoded({ extended: true })); //to parse form data (urlencoded)
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
