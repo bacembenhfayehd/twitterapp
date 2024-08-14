e83f288d836fd5536b43c768b1a9f6756960ad5d
Bacem <Bacem@gmail.com>
2024-08-14 17:25:39 +0100

    add fetching users from data base

--- front/src/components/svgs/common/Post.jsx
+++ front/src/components/svgs/common/Post.jsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import {toast} from 'react-hot-toast'
 import LoadingSpinner from './LoadingSpinner'
+import avatar from '../../../public/avatars/avatar-placeholder.png'
 
 import {useQuery,useMutation, useQueryClient} from '@tanstack/react-query'
 
@@ -62,7 +63,7 @@ const Post = ({ post }) => {
 			<div className='flex gap-2 items-start p-4 border-b border-gray-700'>
 				<div className='avatar'>
 					<Link to={`/profile/${postOwner.username}`} className='w-8 rounded-full overflow-hidden'>
-						<img src={postOwner.profileImg || "/avatar-placeholder.png"} />
+						<img src={postOwner.profileImg || avatar} />
 					</Link>
 				</div>
 				<div className='flex flex-col flex-1'>
--- front/src/components/svgs/common/RightPanel.jsx
+++ front/src/components/svgs/common/RightPanel.jsx
@@ -1,9 +1,37 @@
 import { Link } from "react-router-dom";
 import RightPanelSkeleton from '../skeletons/RightPanelSkeleton';
-import { USERS_FOR_RIGHT_PANEL } from '../../../utils/db/dummy';
+import avatar from '../../../public/avatars/avatar-placeholder.png'
+
+
+import {useQuery} from '@tanstack/react-query'
 
 const RightPanel = () => {
-	const isLoading = false;
+	
+
+	const {data:suggestedUsers,isLoading} = useQuery({
+		queryKey:['suggestedUsers'],
+		queryFn: async  () => {
+			try {
+				const res = await fetch('/api/users/suggested')
+
+				const data =  await res.json();
+
+				if (!res.ok) {
+					throw new Error (data.error || 'Something wrong')
+				}
+
+				return data;
+				
+			} catch (error) {
+				throw  Error(error);
+				
+			}
+
+		}
+	})
+
+
+	if(suggestedUsers.length === 0 ) return <div className="md:w-64 w-0"></div>
 
 	return (
 		<div className='hidden lg:block my-4 mx-2'>
@@ -20,7 +48,7 @@ const RightPanel = () => {
 						</>
 					)}
 					{!isLoading &&
-						USERS_FOR_RIGHT_PANEL?.map((user) => (
+						suggestedUsers?.map((user) => (
 							<Link
 								to={`/profile/${user.username}`}
 								className='flex items-center justify-between gap-4'
@@ -29,7 +57,7 @@ const RightPanel = () => {
 								<div className='flex gap-2 items-center'>
 									<div className='avatar'>
 										<div className='w-8 rounded-full'>
-											<img src={user.profileImg || "/avatar-placeholder.png"} />
+											<img src={user.profileImg || avatar} />
 										</div>
 									</div>
 									<div className='flex flex-col'>
